0a586ced516e35082053b7c532ad1540fd08468c
dqtry <dqpwyyx@163.com>
2017-12-18 23:42:24 +0800

    changed in 610

--- mainTL.m
+++ mainTL.m
@@ -17,11 +17,14 @@ midx=[52 80 23];
 load E:\TransfLearning\PUC\Pu.mat Pu_same Ugt
 ims=reshape(Pu_same,[],size(Pu_same,3));ims_gt=Ugt;
 % [~, ims] = pca(ims,'Centered',true,'NumComponents',5);%,'NumComponents',5
-ims=ims(:,midx);
+% ims=ims(:,midx);
 ims=normcols(ims);
 
 load E:\TransfLearning\PUC\Pc.mat Pc_same Cgt
 imt=reshape(Pc_same(:,224:end,:),[],size(Pc_same,3));imt_gt=Cgt(:,224:end);
 % [~, imt] = pca(imt,'Centered',true,'NumComponents',5);%,'NumComponents',5
-imt=imt(:,midx);
+% imt=imt(:,midx);
 imt=normcols(imt);
+[KL_stmat,KL_tsmat]=CalculateKL_Tree(ims,ims_gt,imt,imt_gt,100);
+Ew(k1,k2)=sqrt(0.5*KL_au+0.5*KL_bu);
+Ew2(k1,k2)=sqrt(min(KL_au,KL_bu));
